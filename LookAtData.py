caff5f0923d370485f4b93fddf460ed18304fa79
ishaan <ishaan.mohan.sinha@gmail.com>
2023-02-27 11:26:41 -0500

    States + CA files

--- LookAtData.py
+++ LookAtData.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from collections import Counter
 
 wnv_data = pd.read_csv('WNV_forecasting_challenge_state-month_cases.csv', index_col=['year', 'month'])
 averageCase = {}
 for i in set(wnv_data['state']):
     averageCase[i] = wnv_data[wnv_data['state'] == i]['count'].mean()
-print(averageCase)
+k = Counter(averageCase)
+
+top3 = k.most_common(3)
+print(top3)
+
 '''
 wnv_data_NJ = wnv_data[wnv_data['state'] == 'NJ']
 
