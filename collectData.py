81db80e72205b173a78e3e008d336c304f73d8c9
ishaan <ishaan.mohan.sinha@gmail.com>
2023-03-09 22:16:33 -0500

    wnv data split

--- collectData.py
+++ collectData.py
@@ -1,12 +1,17 @@
 import pandas as pd
 
 wnv_data = pd.read_csv('WNV_forecasting_challenge_state-month_cases.csv')
-wnv_data_NJ = wnv_data[wnv_data['state'] == 'AZ']
-wnv_data_NJ.index = pd.to_datetime([f'{y}-{m}-01' for y, m in zip(wnv_data_NJ.year, wnv_data_NJ.month)]) #276 x 5
-wnv_data_NJ.index = pd.DatetimeIndex(wnv_data_NJ.index).to_period('M') #276 x 5
 
+
+for i in set(wnv_data['state']):
+    wnv_data_state = wnv_data[wnv_data['state'] == i]
+    wnv_data_state.index = pd.to_datetime([f'{y}-{m}-01' for y, m in zip(wnv_data_state.year, wnv_data_state.month)]) #276 x 5
+    wnv_data_state.index = pd.DatetimeIndex(wnv_data_state.index).to_period('M') #276 x 5
+    wnv_data_state.to_csv(f'{i}/wnv_data_{i}.csv')
+'''
 weather_data = pd.read_csv('AZTemperature.csv', index_col=['Date'])
 
 wnv_data_NJ['temp'] = weather_data['Value'].values
 
 wnv_data_NJ.to_csv('wnv_data_AZ.csv')
+'''
