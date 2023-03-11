741942a2cfbbebbea9cd353d6c4906697f05c8c7
ishaan <ishaan.mohan.sinha@gmail.com>
2023-03-11 15:18:34 -0500

    Webscraper

--- CensusCollater.py
+++ CensusCollater.py
@@ -8,12 +8,11 @@ import os
 wnv_data = pd.read_csv('WNV_forecasting_challenge_state-month_cases.csv', index_col=['year', 'month'])
 c = Census("052c44076be6adb33959d3ee9e8b1c5db81760a6")
 
-
 for state in set(wnv_data['state']):
     dataframes = []
-    for year in range(2009, 2020):
+    for year in range(2009, 2021):
         ca_census = c.acs5.state(fields=(
-        'NAME', 'B01003_001E', 'B01001_002E', 'B01001_026E', 'B01001_003E', 'B01001_004E', 'B01001_005E', 'B01001_006E',
+        'NAME', 'B01001_001E', 'B01001_002E', 'B01001_026E', 'B01001_003E', 'B01001_004E', 'B01001_005E', 'B01001_006E',
         'B01001_007E', 'B01001_008E', 'B01001_009E', 'B01001_010E', 'B01001_011E',
         'B01001_012E', 'B01001_013E', 'B01001_014E', 'B01001_015E', 'B01001_016E',
         'B01001_017E', 'B01001_018E', 'B01001_019E', 'B01001_020E', 'B01001_021E',
@@ -77,6 +76,35 @@ for state in set(wnv_data['state']):
         # B02001_005E	Estimate!!Total:!!Asian alone
         # B02001_006E	Estimate!!Total:!!Native Hawaiian and Other Pacific Islander alone
         ca_df = pd.DataFrame(ca_census)
+        ca_df['male_under_17'] = ca_df['B01001_003E']+ca_df['B01001_004E']+ca_df['B01001_005E']+ca_df['B01001_006E']
+        ca_df['male_18_to_40'] = ca_df['B01001_007E'] + ca_df['B01001_008E'] + ca_df['B01001_009E'] + ca_df['B01001_010E'] + ca_df['B01001_011E'] + ca_df['B01001_012E'] + ca_df['B01001_013E']
+        ca_df['male_40_to_64'] = ca_df['B01001_014E'] + ca_df['B01001_015E'] + ca_df['B01001_016E'] + ca_df['B01001_017E'] + ca_df['B01001_018E'] + ca_df['B01001_019E']
+        ca_df['male_over_65'] = ca_df['B01001_020E'] + ca_df['B01001_021E'] + ca_df['B01001_022E'] + ca_df['B01001_023E'] + ca_df['B01001_024E'] + ca_df['B01001_025E']
+        ca_df['female_under_17'] = ca_df['B01001_027E'] + ca_df['B01001_028E'] + ca_df['B01001_029E'] + ca_df['B01001_030E']
+        ca_df['female_18_to_40'] = ca_df['B01001_031E'] + ca_df['B01001_032E'] + ca_df['B01001_033E'] + ca_df['B01001_034E'] + ca_df['B01001_035E'] + ca_df['B01001_036E'] + ca_df['B01001_037E']
+        ca_df['female_40_to_64'] = ca_df['B01001_038E'] + ca_df['B01001_039E'] + ca_df['B01001_040E'] + ca_df['B01001_041E'] + ca_df['B01001_042E'] + ca_df['B01001_043E']
+        ca_df['female_over_65'] = ca_df['B01001_044E'] + ca_df['B01001_045E'] + ca_df['B01001_046E'] + ca_df['B01001_047E'] + ca_df['B01001_048E'] + ca_df['B01001_049E']
+        ca_df['total_population'] = ca_df['B01001_001E']
+        ca_df['male_population'] = ca_df['B01001_002E']
+        ca_df['female_population'] = ca_df['B01001_026E']
+        ca_df['white_population'] = ca_df['B02001_002E']
+        ca_df['black_population'] = ca_df['B02001_003E']
+        ca_df['native_american_population'] = ca_df['B02001_004E']
+        ca_df['asian_population'] = ca_df['B02001_005E']
+        ca_df['pacific_islander_population'] = ca_df['B02001_006E']
+        ca_df['state'] = state
+        ca_df['year'] = year
+
+        ca_df.drop(['NAME', 'B01001_001E', 'B01001_002E', 'B01001_026E', 'B01001_003E', 'B01001_004E', 'B01001_005E', 'B01001_006E',
+        'B01001_007E', 'B01001_008E', 'B01001_009E', 'B01001_010E', 'B01001_011E',
+        'B01001_012E', 'B01001_013E', 'B01001_014E', 'B01001_015E', 'B01001_016E',
+        'B01001_017E', 'B01001_018E', 'B01001_019E', 'B01001_020E', 'B01001_021E',
+        'B01001_022E', 'B01001_023E', 'B01001_024E', 'B01001_025E','B01001_027E','B01001_028E','B01001_029E','B01001_030E','B01001_031E',
+        'B01001_032E', 'B01001_033E', 'B01001_034E', 'B01001_035E', 'B01001_036E', 'B01001_037E', 'B01001_038E', 'B01001_039E',
+        'B01001_040E', 'B01001_041E', 'B01001_042E', 'B01001_043E', 'B01001_044E', 'B01001_045E', 'B01001_046E', 'B01001_047E',
+        'B01001_048E', 'B01001_049E', 'B02001_002E', 'B02001_003E', 'B02001_004E', 'B02001_005E', 'B02001_006E'], axis=1, inplace=True)
+        ca_df.set_index('year', inplace=True)
         dataframes.append(ca_df)
-    print(dataframes)
-    break
+    total_df = pd.concat(dataframes, axis=0)
+    total_df.sort_index(inplace=True)
+    total_df.to_csv(state + '/census_' + state + '.csv')
--- NOAACollater.py
+++ NOAACollater.py
@@ -1,2 +1,45 @@
-#API Key = yEfVECvHWIzrGqaiNzwaRURsToZbmSoX
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+import time
+import glob
+import os
+import pandas as pd
+
+#token = 'yEfVECvHWIzrGqaiNzwaRURsToZbmSoX'
+'''
+creds = dict(token=token)
+dtype = 'csv'
+
+url = 'https://www.ncei.noaa.gov/cdo-web/api/v2/locations/FIPS:37'
+
+r = requests.get(url, dtype, headers=creds)
+print(r.json())
+'''
+
+id = 'csv-download'
+
+driver = webdriver.Chrome("driver/chromedriver")
+driver.get("https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/statewide/time-series/1/tavg/all/12/1990-2022?base_prd=true&begbaseyear=1901&endbaseyear=2000")
+time.sleep(5)
+
+button = driver.find_element(By.ID, id)
+driver.execute_script("arguments[0].click();", button)
+'''
+'''
+list_of_files = glob.glob('/Users/ishaan/Downloads/*') # * means all if need specific format then *.csv
+latest_file = max(list_of_files, key=os.path.getmtime)
+
+os.rename(latest_file, '/Users/ishaan/PycharmProjects/WNV2023Challenge/fieuwhfwueih.csv')
+
+with open("/Users/ishaan/PycharmProjects/WNV2023Challenge/fieuwhfwueih.csv", "r") as f:
+    data = f.read().split("\n")
+del data[1]
+del data[1]
+del data[1]
+del data[1]
+with open("/Users/ishaan/PycharmProjects/WNV2023Challenge/fieuwhfwueih.csv", "w") as f:
+    f.write("\n".join(data))
+
+noaa_data = pd.read_csv('fieuwhfwueih.csv', index_col=['Date'])
+print(noaa_data)
