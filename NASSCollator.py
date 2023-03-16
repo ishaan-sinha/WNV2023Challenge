df9fdcc725d7977b0d0c02a2f9bbcffb57bd195e
ishaan <ishaan.mohan.sinha@gmail.com>
2023-03-15 22:26:30 -0400

    BLS Collator

---
+++ NASSCollator.py
@@ -0,0 +1,11 @@
+import nass
+'''
+api = nass.NassApi('B664B8B8-C879-3B81-9FB2-A9868A030031')
+
+q = api.query()
+q.filter('sector_desc', 'ECONOMICS')
+q.filter('commodity_desc', 'AG LAND')
+q.filter('category_desc', 'AREA')
+q.filter('data_item', 'COUNTY')
+terminated
+'''
