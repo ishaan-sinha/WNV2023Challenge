1302547c8b3f187f5f6241080cbb1ba9e517e004
ishaan <ishaan.mohan.sinha@gmail.com>
2023-03-16 22:45:03 -0400

    Data collated

--- DataCombiner.py
+++ DataCombiner.py
@@ -1,9 +1,28 @@
+import numpy as np
 import pandas as pd
 
 
 wnv_data = pd.read_csv('WNV_forecasting_challenge_state-month_cases.csv', index_col=['year', 'month'])
-for state in set(wnv_data['state']):
-    state_data = pd.read_csv('states/ '+ str(state) + '/wnv_data_' + str(state)+'.csv', index_col=['year', 'month'])
-    print(state_data)
+for state in [state for state in set(wnv_data['state']) if state != 'DC']:
+    state_wnv = pd.read_csv('states/'+ state.strip() + '/wnv_data_' + state.strip() +'.csv', index_col=['year', 'month'])
+    state_census = pd.read_csv('states/'+ state.strip() + '/census_' + state.strip() +'.csv', index_col=['year'])
+    state_zindex = pd.read_csv('states/'+ state.strip() + '/' + state.strip() +'_zindex_data' + '.csv', index_col=['year', 'month'])
+    state_precipitation = pd.read_csv('states/'+ state.strip() + '/' + state.strip() +'_precipitation_data' + '.csv', index_col=['year', 'month'])
+    state_minTemp = pd.read_csv('states/'+ state.strip() + '/' + state.strip() +'_minTemp_data' + '.csv', index_col=['year', 'month'])
+    state_maxTemp = pd.read_csv('states/'+ state.strip() + '/' + state.strip() +'_maxTemp_data' + '.csv', index_col=['year', 'month'])
+    state_avgTemp = pd.read_csv('states/'+ state.strip() + '/' + state.strip() +'_avgTemp_data' + '.csv', index_col=['year', 'month'])
+    final = state_wnv.copy()
+    final['avgTemp'] = state_avgTemp['Value']
+    final['minTemp'] = state_minTemp['Value']
+    final['maxTemp'] = state_maxTemp['Value']
+    final['precipitation'] = state_precipitation['Value']
+    final['zindex'] = state_zindex['Value']
+    for column in [i for i in state_census.columns if i != 'GEO_ID']:
+        final[column] = np.nan
+        for ind in final.index:
+            if(ind[0] in state_census.index):
+                final.loc[ind, column] = state_census.loc[ind[0], column]
+    final.to_csv('states/'+ state.strip() + '/dropna_final_' + state.strip() +'.csv')
+    print(state)
 
 
--- NOAACollater.py
+++ NOAACollater.py
@@ -15,8 +15,8 @@ id = 'csv-download'
 driver = webdriver.Chrome("driver/chromedriver")
 stateDict = {1:'AL', 2:'AZ', 3:'AR', 4:'CA', 5:'CO',6:'CT',7:'DE',8:'FL',9:'GA',10:'ID',11:'IL',12:'IN',13:'IA',14:'KS',15:'KY',16:'LA',17:'ME',18:'MD',19:'MA',20:'MI',21:'MN',22:'MS',23:'MO',24:'MT',25:'NE',26:'NV',27:'NH',28:'NJ',29:'NM',30:'NY',31:'NC',32:'ND',33:'OH',34:'OK',35:'OR',36:'PA',37:'RI',38:'SC',39:'SD',40:'TN',41:'TX',42:'UT',43:'VT',44:'VA',45:'WA',46:'WV',47:'WI',48:'WY'}
 #only missing DC
-for i in range(41, 49):
-    driver.get("https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/statewide/time-series/"+ str(i) +"/zndx/all/12/1990-2022?base_prd=true&begbaseyear=1901&endbaseyear=2000")
+for i in range(3, 4):
+    driver.get("https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/statewide/time-series/"+ str(i) +"/tmax/all/12/1990-2022?base_prd=true&begbaseyear=1901&endbaseyear=2000")
     state = stateDict[i]
     time.sleep(5)
     button = driver.find_element(By.ID, id)
@@ -31,7 +31,7 @@ for i in range(41, 49):
     del data[0]
     del data[0]
     del data[0]
-    #del data[0]
+    del data[0]
     with open("/Users/ishaan/PycharmProjects/WNV2023Challenge/fieuwhfwueih.csv", "w") as f:
         f.write("\n".join(data))
 
@@ -42,5 +42,5 @@ for i in range(41, 49):
     noaa_data.drop(noaa_data.columns[0], axis=1, inplace=True)
     noaa_data.set_index(['year', 'month'], inplace=True)
 
-    noaa_data.to_csv('states/' + state+'/' + state + '_zindex_data.csv')
+    noaa_data.to_csv('states/' + state+'/' + state + '_maxTemp_data.csv')
     os.remove('/Users/ishaan/PycharmProjects/WNV2023Challenge/fieuwhfwueih.csv')
