4b29b82681bb2a12f7e536778afae382877eb01a
ishaan <ishaan.mohan.sinha@gmail.com>
2023-04-02 17:11:27 -0400

    heatmap 2 for yearly

--- heatMap.py
+++ heatMap.py
@@ -7,9 +7,15 @@ import seaborn as sns
 
 wnv_data = pd.read_csv('WNV_forecasting_challenge_state-month_cases.csv', index_col=['year', 'month'])
 
-#for state in [state for state in set(wnv_data['state']) if state != 'DC']:
-for state in ['AL']:
+for state in [state for state in set(wnv_data['state']) if state != 'DC']:
+#for state in ['CA']:
     state_data = pd.read_csv('states/' + state.strip() + '/withAllInputs_' + state.strip() + '.csv', index_col=[0])
+    state_data = state_data.drop(['male_under_17', 'male_18_to_40', 'male_40_to_64', 'male_over_65',
+       'female_under_17', 'female_18_to_40', 'female_40_to_64',
+       'female_over_65', 'total_population', 'male_population',
+       'female_population', 'white_population', 'black_population',
+       'native_american_population', 'asian_population',
+       'pacific_islander_population', 'household_income'], axis=1)
     state_data.index = pd.DatetimeIndex(state_data.index)
     state_data.index = pd.DatetimeIndex(state_data.index).to_period('M')
 
@@ -23,7 +29,9 @@ for state in ['AL']:
     combined = pandas.DataFrame()
     for year, correlations in yearlydfs:
         combined[year] = correlations
-    print(combined)
     heatMap = sns.heatmap(combined, cmap='coolwarm', xticklabels = True, yticklabels = True)
     #plt.show()
+    plt.title('Correlation Heatmap for ' + state)
     plt.savefig('states/' + state.strip() + '/heatmap_' + state.strip() + '.png')
+    plt.clf()
+    print(state)
