f61c2940053820f99d9bd6fb7ed906e60b7b33ea
ishaan <ishaan.mohan.sinha@gmail.com>
2023-04-20 14:19:30 -0400

    combining data

--- MosquitoData/MosquitoDataAnalysis.py
+++ MosquitoData/MosquitoDataAnalysis.py
@@ -9,11 +9,11 @@ abundanceData = abundanceData[['rate/trap_night', 'year', 'month', 'state', 'sta
 infectionData = pd.read_csv('infection.csv')
 infectionData['inf/pool'] = infectionData['num_pools_wnv']/infectionData['num_mosquitoes']
 infectionData = infectionData[['state', 'statefp', 'countyfp', 'year', 'month', 'species', 'inf/pool']]
-'''
+
 print(infectionData.head())
 print(abundanceData.head())
 
 abundanceData.to_csv('abundanceData.csv')
 infectionData.to_csv('infectionData.csv')
-'''
+
 print(set(abundanceData['state']))
--- MosquitoData/countyHeatMap.py
+++ MosquitoData/countyHeatMap.py
@@ -10,7 +10,7 @@ for row in abundanceData.iterrows():
 fips = list(set(fips))
 
 values = [5 for i in range(len(fips))]
-
+2
 fig = ff.create_choropleth(fips=fips, values=values)
 
 fig.layout.template = None
