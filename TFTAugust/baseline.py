8dcc7506542af434076ef92b547c00496f40176d
ishaan <ishaan.mohan.sinha@gmail.com>
2023-07-23 17:49:50 -0400

    added all files

--- TFTAugust/baseline.py
+++ TFTAugust/baseline.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pickle
 import os
 
-os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+#os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 
 EPOCHS = 3
 INLEN = 32
@@ -123,7 +123,7 @@ for state in ['CA']:
                      pl_trainer_kwargs={
                          "accelerator": "gpu",
                          "devices": [0],
-                         "precision": '32-true'
+                         #"precision": '32-true'
                      },
                      force_reset=True)
 
