422da4d46d848836b156c7becd39413617e645ff
ishaan <ishaan.mohan.sinha@gmail.com>
2023-07-29 16:02:38 -0400

    created copy of logistic fitted

--- TFTAugust/baselineWithNationalandWikiandLogistic.py
+++ TFTAugust/baselineWithNationalandWikiandLogistic.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 import os
-from SIRModel.logistic_fitted import getLogisticPrediction
+from logistic_fitted import getLogisticPrediction
 #os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 
 EPOCHS = 300
