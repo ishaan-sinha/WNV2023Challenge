c7598cef5f7981f6d88ce5703b6e68987cbc1c66
ishaan <ishaan.mohan.sinha@gmail.com>
2023-08-31 11:14:22 -0400

    put on gpu 3

--- TFTSeptember/baselineWithWikiandLogistic.py
+++ TFTSeptember/baselineWithWikiandLogistic.py
@@ -134,7 +134,7 @@ for state in [i for i in wnv_data['state'].unique() if i not in ['DC']]:
                      random_state=RAND,
                      pl_trainer_kwargs={
                          "accelerator": "gpu",
-                         "devices": [2],
+                         "devices": [3],
                          "precision": '32-true'
                      },
                      force_reset=True)
