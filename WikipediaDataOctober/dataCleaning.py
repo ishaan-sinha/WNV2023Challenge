41e8def314eb459b89140669bd91fece896c1c87
ishaan <ishaan.mohan.sinha@gmail.com>
2023-10-02 14:02:20 -0400

    baseline with wiki

---
+++ WikipediaDataOctober/dataCleaning.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+files = ['wikiDataOctober.csv']
+
+for file in files:
+    currentFile = pd.read_csv('raw/'+file)
+    currentFile.index = pd.to_datetime(currentFile['DateTime'])
+    currentFile.drop(['DateTime'], axis=1, inplace=True)
+    currentFile = currentFile.groupby(pd.Grouper(freq='M')).sum()
+    currentFile.to_csv('Monthly'+file, index=True)
