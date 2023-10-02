7613b035107b981d720cf1c0ac23c85cd71b1b4e
ishaan <ishaan.mohan.sinha@gmail.com>
2023-10-02 13:52:03 -0400

    wnv data for October

--- collectData.py
+++ collectData.py
@@ -8,7 +8,7 @@ for i in set(wnv_data['state']):
     wnv_data_state = wnv_data[wnv_data['state'] == i]
     wnv_data_state.index = pd.to_datetime([f'{y}-{m}-01' for y, m in zip(wnv_data_state.year, wnv_data_state.month)]) #276 x 5
     wnv_data_state.index = pd.DatetimeIndex(wnv_data_state.index) #276 x 5
-    wnv_data_state.to_csv('statesSeptemberSubmission/'+i+'/wnv_data.csv')
+    wnv_data_state.to_csv('statesOctoberSubmission/'+i+'/wnv_data.csv')
 
 '''
 weather_data = pd.read_csv('AZTemperature.csv', index_col=['Date'])
